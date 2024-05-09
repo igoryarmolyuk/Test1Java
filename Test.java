a32af299648bc1e3b973a4b82487de98a0ee4fef
igoryarmolyuk <igor.yarmolyuk00@gmail.com>
2024-05-09 18:33:52 +0300

    Homework

--- Test.java
+++ Test.java
@@ -1,6 +1,47 @@
 // https://github.com/igoryarmolyuk/Test1Java/tree/master
 public class Test {
     public static void main(String[] args) {
-        System.out.println("that was really hard!");
+        int[] bubble = {4, 7, 5, 6};
+        int[] insert = {3, 7, 6, 5};
+        print(bubble);
+        // Сортування бульбашкою
+        bubbleSort(bubble);
+        // Сортування вставками
+        print(insert);
+        insertSort(insert);
+    }
+
+    public static void bubbleSort(int[] ar) {
+        for (int i = 0; i < ar.length; i++) {
+            for (int j = 0; j < ar.length - 1; j++) {
+                if (ar[j] > ar[j + 1]) {
+                    int tmp = ar[j];
+                    ar[j] = ar[j + 1];
+                    ar[j + 1] = tmp;
+                }
+            }
+        }
+    }
+
+    public static void insertSort(int[] ar) {
+        for (int i = 1; i < ar.length; i++) {
+            int key = ar[i];
+            int j;
+            for (j = i - 1; j >= 0 && ar[j] > key; j--) {
+                ar[j + 1] = ar[j];
+            }
+            ar[j + 1] = key;
+        }
+    }
+
+
+    public static void print(int[] ar) {
+        for (int i = 0; i < ar.length; i++) {
+            System.out.print(ar[i]);
+            if (i != ar.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
     }
 }
--- Test2.java
+++ Test2.java
@@ -1,37 +1,17 @@
-// unfinished https://github.com/igoryarmolyuk/Test1Java/tree/master
-import javax.sound.midi.*;
-
 public class Test2 {
     public static void main(String[] args) {
-        try {
-            // Open a sequencer
-            Sequencer sequencer = MidiSystem.getSequencer();
-            sequencer.open();
-
-            // Open a synthesizer
-            Synthesizer synthesizer = MidiSystem.getSynthesizer();
-            synthesizer.open();
-
-            // Get the default MIDI channel (usually 0)
-            MidiChannel midiChannel = synthesizer.getChannels()[0];
-
-            // Define the notes you want to play simultaneously (in MIDI note numbers)
-            int[] notes = {64, 67}; // Example: C Major chord
-
-            // Play each note
-            for (int note : notes) {
-                // Send a NOTE_ON message for each note
-                midiChannel.noteOn(note, 100); // 100 is the velocity (volume)
+        triangle();
+    }
+    public static void triangle() {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j == 5 && i<4 || i+5 == j && i<4 || i == 3 && j == 6 || i==3 && j == 7) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
             }
-
-            // Sleep to allow the notes to play
-            Thread.sleep(2000); // Adjust this value as needed
-
-            // Close the sequencer and synthesizer
-            sequencer.close();
-            synthesizer.close();
-        } catch (MidiUnavailableException | InterruptedException e) {
-            e.printStackTrace();
+            System.out.println();
         }
     }
 }
